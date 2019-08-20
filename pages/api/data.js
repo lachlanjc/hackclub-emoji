181517b7e427d4e3b8650c86cdb8074dcb752fc3
Lachlan Campbell <lachlan@hackclub.com>
2019-08-20 16:48:47 -0400

    Correctly sort reactions on channels + users

--- pages/api/data.js
+++ pages/api/data.js
@@ -5,6 +5,11 @@ const dataPath = 'https://emoji-ranker.now.sh/data'
 const channelsPath = 'https://emoji-ranker.now.sh/channeldata'
 const customPath = 'https://emoji-ranker.now.sh/custompng'
 
+const sortReactions = a =>
+  Object.entries(a)
+    .sort((a, b) => b[1] - a[1])
+    .reduce((o, [k, v]) => ((o[k] = v), o), {})
+
 export default async (req, res) => {
   let custom = await fetch(customPath).then(data => data.json())
   custom = pickBy(custom, (value, key) => !includes(value, 'alias'))
@@ -17,21 +22,23 @@ export default async (req, res) => {
     uuid,
     name,
     reactionsTotal: reactions.total,
-    reactions: omit(reactions, 'total')
+    reactions: sortReactions(omit(reactions, 'total'))
   }))
 
   let channels = await fetch(channelsPath).then(data => data.json())
   channels = pickBy(
     channels,
     channel =>
-      channel.reactions.total > 1 && channel.name.split(' ')[0] === channel.name
+      channel.reactions.total > 1 &&
+      channel.name.split(' ')[0] === channel.name &&
+      !includes(channel.name, 'spam')
   )
   channels = Object.values(channels)
   channels = channels.map(({ uuid, name, reactions }) => ({
     uuid,
     name,
     reactionsTotal: reactions.total,
-    reactions: omit(reactions, 'total')
+    reactions: sortReactions(omit(reactions, 'total'))
   }))
 
   let top = {}
@@ -39,7 +46,7 @@ export default async (req, res) => {
     .map(user => user.reactions)
     .map(reactionSet => {
       Object.keys(reactionSet).map(id => {
-        // either add to top or increase quanity in top
+        // either add to top or increase quantity in top
         if (has(top, id)) {
           top[id] += reactionSet[id]
         } else {
@@ -49,10 +56,7 @@ export default async (req, res) => {
     })
   // filter for >1
   top = pickBy(top, (value, key) => value > 1)
-  // sort by amount
-  top = Object.entries(top)
-    .sort((a, b) => b[1] - a[1])
-    .reduce((o, [k, v]) => ((o[k] = v), o), {})
+  top = sortReactions(top)
 
   const json = { top, channels, users, custom }
   res.json(json)
